dbba19eed1ad0130c086038c0f4dccda27d78e81
daCruzZz_Leticia <dacruzzzleticia@gmail.com>
2025-10-21 08:56:39 -0300

    feat: #1000 - Hello World! (C++)

---
+++ 1_iniciante_beginner/c++/1000.cpp
@@ -0,0 +1,17 @@
+/**
+ * @source Beecrowd
+ * @problem 1000 - Hello World!
+ * @subject Sequential
+ *
+ * @author Letícia Cruz
+ * @date 2025-08-25
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
