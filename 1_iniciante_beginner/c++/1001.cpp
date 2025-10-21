a0062d21b40a96213a75a7f1a742cf3ca76845fb
daCruzZz_Leticia <dacruzzzleticia@gmail.com>
2025-10-21 08:58:23 -0300

    feat: #1001 - Extremely Basic (C++)

---
+++ 1_iniciante_beginner/c++/1001.cpp
@@ -0,0 +1,24 @@
+/**
+ * @source Beecrowd
+ * @problem 1001 - Extremely Basic
+ * @subject Sequential
+ *
+ * @author Letícia Cruz
+ * @date 2025-08-25
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int a, b, x;
+    cin >> a >> b;
+
+    x = a + b;
+    cout << "X = " << x << endl;
+
+    return 0;
+}
